fd9932230880a5d7f7cf8faed30773a058dbc838
oiuv <i@oiuv.cn>
2018-06-09 22:55:08 +0800

    Finish basic page layout

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-sm navbar-light bg-light navbar-static-top">
+    <div class="container">
+        <div class="navbar-brand">
+            <!-- Collapsed Hamburger -->
+            <button type="button" class="navbar-toggler" data-toggle="collapse"
+                    data-target="#app-navbar-collapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <!-- Branding Image -->
+            <a class="navbar-brand" href="{{ url('/') }}">
+                LaraBBS
+            </a>
+        </div>
+
+        <div class="collapse navbar-collapse justify-content-end" id="app-navbar-collapse">
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav nav-pills">
+                <!-- Authentication Links -->
+                <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">注册</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
