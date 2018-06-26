1b976185c5b0cb5368c9cfdfa105b85f50692b40
oiuv <i@oiuv.cn>
2018-06-26 23:08:29 +0800

    新增个人中心菜单

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -30,6 +30,7 @@
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('users.show', Auth::id()) }}">个人中心</a>
                             <a class="dropdown-item" href="{{ route('users.edit', Auth::id()) }}">编辑资料</a>
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
