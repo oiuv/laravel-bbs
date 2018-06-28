00bd0238806e5202bafd31c5fe1b15a888d9fa10
oiuv <i@oiuv.cn>
2018-06-28 09:35:42 +0800

    Seeding topics and users data

--- database/migrations/2018_06_28_002302_create_topics_table.php
+++ database/migrations/2018_06_28_002302_create_topics_table.php
@@ -3,16 +3,16 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTopicsTable extends Migration 
+class CreateTopicsTable extends Migration
 {
-	public function up()
-	{
-		Schema::create('topics', function(Blueprint $table) {
+    public function up()
+    {
+        Schema::create('topics', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title')->index();
             $table->text('body');
-            $table->integer('user_id')->unsigned()->index();
-            $table->integer('category_id')->unsigned()->index();
+            $table->integer('user_id')->unsigned()->index()->nullable();
+            $table->integer('category_id')->unsigned()->index()->default(1);
             $table->integer('reply_count')->unsigned()->default(0);
             $table->integer('view_count')->unsigned()->default(0);
             $table->integer('last_reply_user_id')->unsigned()->default(0);
@@ -21,10 +21,10 @@ class CreateTopicsTable extends Migration
             $table->string('slug')->nullable();
             $table->timestamps();
         });
-	}
+    }
 
-	public function down()
-	{
-		Schema::drop('topics');
-	}
+    public function down()
+    {
+        Schema::drop('topics');
+    }
 }
