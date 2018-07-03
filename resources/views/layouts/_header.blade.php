685d97ed2a7084d3b4e1861f20bf44f4a240d79b
oiuv <i@oiuv.cn>
2018-07-04 01:11:01 +0800

    User can create topic

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -28,6 +28,11 @@
                         <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                     </li>
                 @else
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('topics.create') }}">
+                            <span class="fa fa-plus" aria-hidden="true"></span>
+                        </a>
+                    </li>
                     <li class="nav-item dropdown">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             <img class="img-responsive img-circle" src="{{ Auth::user()->avatar }}" height="30px" width="30px">{{ Auth::user()->name }} <span class="caret"></span>
--- resources/views/topics/_sidebar.blade.php
+++ resources/views/topics/_sidebar.blade.php
@@ -1,5 +1,7 @@
 <div class="card">
     <div class="card-body">
-        右边导航栏
+        <a href="{{ route('topics.create') }}" class="btn btn-success btn-block" aria-label="Left Align">
+            <span class="fa fa-pencil" aria-hidden="true"></span> 新建帖子
+        </a>
     </div>
 </div>
