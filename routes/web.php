699fb95a8c5f254d33bba7a96c90a15b60b34d86
oiuv <i@oiuv.cn>
2018-07-06 20:47:27 +0800

    List user replies

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,4 @@ $this->get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show
 $this->resource('categories', 'CategoriesController', ['only' => ['show']]);
 
 $this->post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
