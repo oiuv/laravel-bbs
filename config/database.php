a77653419909b990f5443e5b60bf2b44974e0ec2
oiuv <i@oiuv.cn>
2018-07-16 23:27:58 +0800

    update faker composer

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
             'strict' => true,
             'engine' => null,
         ],
-        
+
         'pgsql' => [
             'driver'   => 'pgsql',
             'host'     => $db_config['host'],
